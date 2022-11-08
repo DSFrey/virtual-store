1275ec1d27f24f63abe301f8884bfce2a3b5762c
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-08 00:24:40 +0000

    Setup redux store for categories and products

---
+++ src/Store/categories.js
@@ -0,0 +1,29 @@
+const initialState = {
+  categories: [
+    { name: 'electronics', displayName: 'Electronics' },
+    { name: 'food', displayName: 'Food' },
+    { name: 'clothing', displayName: 'Clothing' },
+  ],
+  activeCategory: ''
+}
+
+export const categoryReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch(type) {
+    case 'SELECT':
+      return {
+        ...state,
+        activeCategory: state.categories.find(element => element.name === payload)
+      }
+    default:
+      return state;
+  }
+}
+
+export const selectCategory = (category) => {
+  return {
+    type: 'SELECT',
+    payload: category
+  }
+}
